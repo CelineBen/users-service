3784876dd43737aa522d09ecc84b03fd128c3426
Quentin Ventura <ventura.quentin@gmail.com>
2018-06-21 14:44:38 -0400

    Adding .gitignore and removing extra commited files

--- dist/src/index.js
+++
@@ -1,9 +0,0 @@
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
-var app = new Koa();
-var router = new Router();
-router.get('/health', function (ctx, next) {
-    ctx.response.status = 200;
-});
-app.listen(5000);
-//# sourceMappingURL=index.js.map
